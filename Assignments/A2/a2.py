2f386424b14c81322562dd38674d112873dc2f43
Ryan Barclay <mrryanbarclay@gmail.com>
2021-02-17 19:12:22 -0800

    Init of a2

---
+++ Assignments/A2/a2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# Not allowed to use any big boy libraries
+
+    """Assignment 2 
+
+    TBA
+    """
+
+import socket
+import sys
+import re
+import ssl
+DEFAULT_HTTP_PORT = 80
